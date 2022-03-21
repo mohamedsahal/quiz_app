70d4fff256095087f38414729fdac1c5da15de12
mohamedsahal <mohaansahal@gmail.com>
2022-03-21 17:57:25 +0300

    Creating Question Class and Handle errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/question.dart';
 
 void main() {
   runApp(
@@ -17,39 +18,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> questions = [
-    "Muslims believe that Muhammad was one of many prophets.",
-    "After Muhammad started receiving revelations, his preaching was immediately welcomed by most of the people of Mecca.",
-    "The hijra is the term for the migration of Muhammad and his followers from Mecca to Cairo.",
-    "The language of the Qur'an is Arabic.",
-    "There is only one source of Islamic law—the Sunnah.",
-    "It is the goal of Sufis to become prophets.",
-    "Muslims believe in the coming Day of Judgment.",
-    "All Muslims today believe that polygamy is ideal marriage.",
-    "During the month of Ramadan, devout Muslims make the pilgrimage to Mecca.",
-    "One of the Five Pillars is the duty of able Muslims to make the pilgrimage to Baghdad.",
-    "There are many sources of shari'ah, including the Qur'an and the Sunnah.",
-    "During the miraj, or Night Journey, Muhammad ascended to heaven and came into the presence of God.",
-    "Islamic law is no longer used in the world today.",
-    "Muslims reformers in the modern period were not concerned about European colonialism.",
-    "Most African-American Muslims belong to the Nation of Islam."
-  ];
-  List<bool> answers = [
-    true,
-    false,
-    false,
-    true,
-    false,
-    false,
-    true,
-    false,
-    false,
-    false,
-    true,
-    true,
-    false,
-    false,
-    false,
+  List<question> questions = [
+    question(
+        q: "Muslims believe that Muhammad was one of many prophets.", a: true),
+    question(
+        q: "After Muhammad started receiving revelations, his preaching was immediately welcomed by most of the people of Mecca.",
+        a: false),
+    question(
+        q: "The hijra is the term for the migration of Muhammad and his followers from Mecca to Cairo.",
+        a: false),
+    question(q: "The language of the Qur'an is Arabic.", a: true),
+    question(
+        q: "There is only one source of Islamic law—the Sunnah.", a: false),
+    question(q: "It is the goal of Sufis to become prophets.", a: false),
+    question(q: "Muslims believe in the coming Day of Judgment.", a: true),
+    question(
+        q: "All Muslims today believe that polygamy is ideal marriage.",
+        a: false),
+    question(
+        q: "During the month of Ramadan, devout Muslims make the pilgrimage to Mecca.",
+        a: false),
+    question(
+        q: "One of the Five Pillars is the duty of able Muslims to make the pilgrimage to Baghdad.",
+        a: false),
+    question(
+        q: "There are many sources of shari'ah, including the Qur'an and the Sunnah.",
+        a: true),
+    question(
+        q: "During the miraj, or Night Journey, Muhammad ascended to heaven and came into the presence of God.",
+        a: true),
+    question(q: "Islamic law is no longer used in the world today.", a: false),
+    question(
+        q: "Muslims reformers in the modern period were not concerned about European colonialism.",
+        a: false),
+    question(
+        q: "Most African-American Muslims belong to the Nation of Islam.",
+        a: false),
   ];
   int questionindex = 0;
   @override
@@ -62,7 +66,7 @@ class _HomeState extends State<Home> {
             flex: 5,
             child: Center(
               child: Text(
-                questions[questionindex],
+                questions[questionindex].questionName!,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   letterSpacing: 2.0,
@@ -85,9 +89,11 @@ class _HomeState extends State<Home> {
                           primary: Color(0xff06d6a0),
                         ),
                         onPressed: () {
-                          final isTrue = answers[questionindex];
+                          final isTrue =
+                              questions[questionindex].questionAnswer;
                           setState(() {
-                            questionindex++;
+                            if (questionindex < questions.length - 1)
+                              questionindex++;
                           });
                           if (isTrue == true) {
                             print('Waa Saxday');
@@ -117,9 +123,11 @@ class _HomeState extends State<Home> {
                           primary: Color(0xffef476f),
                         ),
                         onPressed: () {
-                          final isTrue = answers[questionindex];
+                          final isTrue =
+                              questions[questionindex].questionAnswer;
                           setState(() {
-                            questionindex++;
+                            if (questionindex < questions.length - 1)
+                              questionindex++;
                           });
                           if (isTrue == true) {
                             print('Waa Saxday');
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class question {
+  String? questionName;
+  bool? questionAnswer;
+
+  question({String? q, bool? a}) {
+    questionName = q;
+    questionAnswer = a;
+  }
+}
